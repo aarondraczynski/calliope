f695c6b0289e6112d70ae796beb1e5508dcd007c
Aaron Draczynski <aarondraczynski@gmail.com>
2013-05-28 21:18:32 -0700

    Workaround for multiple route directions. More code comments.

--- calliope.js
+++ calliope.js
@@ -7,21 +7,39 @@ function getPredictions() {
           firstRun = $('body').hasClass('run'),
           predictions = false;
 
+      // For each route we're retrieving data for
       for (var i = 0; i < data.length; i++) {
         var count = i + 1;
 
+        // Populate route letter/number
         $('#route-' + count + '-id').text(data[i].routeTag);
+
+        // If predictions are available for this route
         if (typeof data[i].direction !== 'undefined') {
-          $('#route-' + count + '-direction').text(data[i].direction.title);
+
+          // Remove old predictions from page
           $('#route-' + count + ' .route-predictions').find('.no-predictions').hide().end().children().not('.no-predictions').remove();
 
+          // If multiple route directions exist for this route, use the last one
+          if (data[i].direction.length > 1) {
+            data[i].direction = data[i].direction[data[i].direction.length - 1];
+          }
+
+          // Populate route direction
+          $('#route-' + count + '-direction').text(data[i].direction.title);
+
+          // For each available prediction
           if (data[i].direction.prediction.length > 1) {
+
             for (var j = 1; j <= ((data[i].direction.prediction.length >= 3) ? 3 : data[i].direction.prediction.length); j++) {
+
+              // Render prediction
               var prediction = '<span';
               if (firstRun) {
                 prediction += ' class="hidden"';
               }
               prediction += '>' + data[i].direction.prediction[j - 1].minutes;
+              // Append comma if this is not the last prediction of the series
               if (j !== ((data[i].direction.prediction.length >= 3) ? 3 : data[i].direction.prediction.length)) {
                 prediction += ',';
               }
@@ -29,6 +47,7 @@ function getPredictions() {
 
               $('#route-' + count + ' .route-predictions').append(prediction);
             }
+
             predictions = true;
           } else {
             var prediction = '<span';
@@ -38,16 +57,20 @@ function getPredictions() {
             prediction += '>' + data[i].direction.prediction.minutes + '</span>';
 
             $('#route-' + count + ' .route-predictions').append(prediction);
+
             predictions = true;
           }
         } else {
+          // No predictions available for this route, so show emdash
           $('#route-' + count + ' .route-predictions').find('.no-predictions').show().end().children().not('.no-predictions').remove();
           $('#route-' + count + '-direction').text(data[i].dirTitleBecauseNoPredictions);
         }
 
+        // Populate stop location name
         $('#route-' + count + '-stop').text(data[i].stopTitle);
       }
 
+      // Animate predictions on first run
       if (firstRun) {
         if (predictions) {
           $('.route-predictions span').not('.no-predictions').each(function(i, elem) {
